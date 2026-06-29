eba02c1df8353df7d96af6cd053e602e6180bb53
Lipsarani Sankhua <sankhualipsarani51@gmail.com>
2026-06-29 16:00:01 +0530

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,32 @@
+//This script calculates the discounted price of items in an array and logs it to the console.
+let items = [250, 645, 300, 900, 50];
+
+for (let i = 0; i < items.length; i++) {
+    let discountedItem = items[i] - items[i] * 10 / 100;
+    console.log(discountedItem);
+}
+
+
+
+let arr = ["Lipsarani"];
+arr.forEach((val) => {
+   console.log(val.toLowerCase(), arr);
+})
+
+
+
+let users = ["Lipsa", "Radha", "Anita"];
+users.forEach((value) => {
+   console.log("Hello, Lipsa");
+
+
+
+
+
+
+const celsiusToFahrenheit = (celsius) => celsius * 1.8 +32;
+console.log(celsiusToFahrenheit(100));
+
+
+
+
