35ce1ea24d6ac52ca3d3c4a6545f4f7c68dfd056
Lipsarani Sankhua <sankhualipsarani51@gmail.com>
2026-06-29 16:00:51 +0530

    Add files via upload

---
+++ script_02.js
@@ -0,0 +1,8 @@
+//pop method
+let foodItems = ["Pizza", "Burger", "Pasta", "Salad", "Sushi"];
+
+console.log(foodItems);
+
+let deleteItem = foodItems.pop();
+console.log(foodItems);
+console.log("Deleted item: " ,deleteItem);
